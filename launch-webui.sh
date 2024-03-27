67ee92ac86531669e0ec5d8070debdc984efadb4
Abdullah Al Mamun <m98.aam@gmail.com>
2024-03-27 11:39:38 +0600

    Update launch-webui.sh

--- launch-webui.sh
+++ launch-webui.sh
@@ -3,7 +3,7 @@
 eval "$(conda shell.bash hook)"
 conda activate automatic
 
-cd ~/stable-diffusion-webui
+cd ~/stable-diffusion/stable-diffusion-webui
 git pull
 read -p "Enter Ngrok Authtoken: " NGROK_AUTHTOKEN
 python launch.py --ngrok ${NGROK_AUTHTOKEN} --listen --xformers --no-half-vae --enable-insecure-extension-access --theme dark --gradio-queue
