72d7c882a7d308b52cd54c0178b324532eb8aae2
Abdullah Al Mamun <m98.aam@gmail.com>
2024-03-27 18:51:57 +0600

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -7,12 +7,5 @@ cd ~/stable-diffusion/stable-diffusion-webui
 git pull
 read -p "Enter Ngrok Authtoken: " NGROK_AUTHTOKEN
 python launch.py --ngrok ${NGROK_AUTHTOKEN} --listen --xformers --no-half-vae --enable-insecure-extension-access --theme dark --gradio-queue
-cd ~
-
-rm -rf ~/.cache/*
-rm -rf ~/stable-diffusion/stable-diffusion-webui/output/*
-
-df -h | grep -E 'Avail|home'
-du -h --max-depth=1 ~
 
 conda deactivate
