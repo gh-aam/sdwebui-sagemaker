5e83a06b96f689a7587425c20f72e3ca8203e33e
Abdullah Al Mamun <m98.aam@gmail.com>
2024-03-27 18:45:38 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,11 @@ conda create -y -n automatic python=3.10 glib=2.51.0
 eval "$(conda shell.bash hook)"
 conda activate automatic
 
-mkdir -p ~/stable-diffusion
+cd ~
+mkdir stable-diffusion
 
-cd ~/stable-diffusion && git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git && wget https://github.com/gh-aam/sdwebui-sagemakersl/raw/main/webui.sh
+cd ~/stable-diffusion
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+wget https://github.com/gh-aam/sdwebui-sagemakersl/raw/main/webui.sh
 
 conda deactivate
