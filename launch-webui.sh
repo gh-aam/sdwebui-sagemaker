d1114e477ab3bac99ae60c88feb128d6ba5b61fa
Abdullah Al Mamun <m98.aam@gmail.com>
2024-03-27 11:58:49 +0600

    Update launch-webui.sh

--- launch-webui.sh
+++ launch-webui.sh
@@ -7,8 +7,8 @@ cd ~/stable-diffusion/stable-diffusion-webui
 git pull
 read -p "Enter Ngrok Authtoken: " NGROK_AUTHTOKEN
 python launch.py --ngrok ${NGROK_AUTHTOKEN} --listen --xformers --no-half-vae --enable-insecure-extension-access --theme dark --gradio-queue
-
 cd ~
+
 rm -rf ~/.cache/*
 rm -rf ~/stable-diffusion/stable-diffusion-webui/output/*
 df -h | grep -E 'Avail|home'
