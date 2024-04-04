30b2036863674e4caed9b6c6e0f850f1456b49fb
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-04 12:28:42 +0600

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,7 +5,22 @@ conda activate sdwebui
 
 cd stable-diffusion-webui
 git pull
-read -p "Enter Ngrok token: " NGROK_TOKEN
+
+cd ..
+
+if [[ "$1" == "--reset" ]]; then
+    read -p "Enter Ngrok token: " NGROK_TOKEN
+    echo "$NGROK_TOKEN" > "ngrok_data.txt"
+fi
+
+if [[ -f "ngrok_data.txt" ]]; then
+    NGROK_TOKEN=$(cat "ngrok_data.txt")
+else
+    read -p "Enter Ngrok token: " NGROK_TOKEN
+    echo "$NGROK_TOKEN" > "ngrok_data.txt"
+fi
+
+cd stable-diffusion-webui
 python launch.py --ngrok $NGROK_TOKEN --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue --theme dark
 
 conda deactivate
