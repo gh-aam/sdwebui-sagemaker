2acfa3d41de3ad6d089db5e997c1f2287c09be00
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-04 11:12:52 +0600

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@ eval "$(conda shell.bash hook)"
 conda activate automatic
 
 git pull
-read -p "Enter Ngrok Authtoken: " NGROK_AUTHTOKEN
+read -p "Enter Ngrok token: " NGROK_AUTHTOKEN
 python launch.py --ngrok $NGROK_AUTHTOKEN --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue --theme dark
 
 conda deactivate
