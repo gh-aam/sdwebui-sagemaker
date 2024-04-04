7580612fdf15d60c38e9d35352d01324047d9fff
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-04 12:03:22 +0600

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+eval "$(conda shell.bash hook)"
+conda activate sdwebui
+
 cd stable-diffusion-webui
 git pull
-
-eval "$(conda shell.bash hook)"
-conda activate automatic
-
 read -p "Enter Ngrok token: " NGROK_TOKEN
 python launch.py --ngrok $NGROK_TOKEN --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue --theme dark
 
