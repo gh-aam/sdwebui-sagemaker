09afa3cdcb8c8e635239ee7ce730f5ac5e95c959
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-04 12:08:28 +0600

    Update storage.sh

--- storage.sh
+++ storage.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+eval "$(conda shell.bash hook)"
+conda activate sdwebui
+
 rm -rf ~/.cache/*
 rm -rf stable-diffusion-webui/output/*
 
 df -h | grep -E 'Avail|home'
 du -h --max-depth=1 ~
+
+conda deactivate
