18417339e0af822fb50780d2af319668826efcb2
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-05 18:07:03 +0600

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -7,5 +7,3 @@ cd stable-diffusion-webui
 git pull
 read -p "Enter Ngrok token: " NGROK_TOKEN
 python launch.py --ngrok $NGROK_TOKEN --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue --theme dark
-
-conda deactivate
