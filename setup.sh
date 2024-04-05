3241d452ba3efc224b3511ea33cc683b4f581a8e
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-05 19:22:30 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-conda create -y -n sdwebui python=3.10 glib
-eval "$(conda shell.bash hook)"
-conda activate sdwebui
-
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
-
-conda deactivate
+conda create -y -n automatic python=3.10 glib
