a11842852480aa01da8b3120b539bce3087c3126
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-06 08:50:16 +0600

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -9,3 +9,6 @@ git pull
 conda create -y -n automatic python=3.10 glib
 eval "$(conda shell.bash hook)"
 conda activate automatic
+
+read -p "Enter Ngrok token: " NGROK_TOKEN
+python launch.py --ngrok $NGROK_TOKEN --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue
