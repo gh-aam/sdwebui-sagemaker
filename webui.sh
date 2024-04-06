66bf12d9915eb1df62d9a80bd3b94c12a1ee9546
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-06 09:22:35 +0600

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+if [ ! -d "~/.conda/envs/automatic" ]; then
+    conda create -y -n automatic python=3.10 glib
+fi
+
 if [ ! -d "stable-diffusion-webui" ]; then
     git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 fi
-cd stable-diffusion-webui
-git pull
-
-conda create -y -n automatic python=3.10 glib
-eval "$(conda shell.bash hook)"
-conda activate automatic
 
 read -p "Enter Ngrok token: " NGROK_TOKEN
+
+cd stable-diffusion-webui
+git pull
+eval "$(conda shell.bash hook)"
+conda activate automatic
 python launch.py --ngrok $NGROK_TOKEN --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue
