d987f2e6d5ff1141e8f2d118b86282e94ba41aba
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-06 08:49:55 +0600

    Delete launch.sh

--- launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd stable-diffusion-webui
-git pull
-eval "$(conda shell.bash hook)"
-conda activate sdwebui
-read -p "Enter Ngrok token: " NGROK_TOKEN
-python launch.py --ngrok $NGROK_TOKEN --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue
