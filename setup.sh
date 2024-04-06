a34ed4fe21c38b79b288d961a305dd49dae442a7
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-06 08:01:56 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 conda create -y -n sdwebui python=3.10 glib
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
