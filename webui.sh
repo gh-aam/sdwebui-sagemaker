f76f7a2194e442712290441ab5f086b658a3521b
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-08 23:24:09 +0600

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -8,9 +8,28 @@ if [ ! -d "stable-diffusion-webui" ]; then
     git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 fi
 
+SCRIPT_DIR=$(pwd)
 cd stable-diffusion-webui
 git pull
-read -p "Enter Ngrok token: " NGROK_TOKEN
+
+if [ ! -f "$SCRIPT_DIR/ngrok_data.txt" ]; then
+    read -p "Enter Ngrok token: " TOKEN
+    echo $TOKEN > $SCRIPT_DIR/ngrok_data.txt
+else
+    read -p "Ngrok token found! Reset? (Y/N/E): " RESET_OPTION
+    case "$RESET_OPTION" in
+        [yY]*)
+            read -p "Enter Ngrok token: " TOKEN
+            echo $TOKEN > $SCRIPT_DIR/ngrok_data.txt
+            ;;
+        [nN]*)
+            ;;
+        *)
+            ;;
+    esac
+fi
+
+NGROK_TOKEN=$(cat $SCRIPT_DIR/ngrok_data.txt)
 eval "$(conda shell.bash hook)"
 conda activate automatic
-python launch.py --ngrok $NGROK_TOKEN --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue
+python launch.py --ngrok $NGROK_TOKEN --api --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue --theme dark
