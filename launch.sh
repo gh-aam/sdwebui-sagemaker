991c532fb1430a5d44244385db02a170f087588d
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-10 08:40:11 +0600

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,18 +4,18 @@ cd stable-diffusion-webui
 git pull
 
 cd ..
-if [ ! -f "$SCRIPT_DIR/ngrok_data.txt" ]; then
+if [ ! -f "ngrok_data.txt" ]; then
     read -p "Enter Ngrok token: " NGROK_TOKEN
-    echo $NGROK_TOKEN > $SCRIPT_DIR/ngrok_data.txt
+    echo $NGROK_TOKEN > ngrok_data.txt
 else
     read -p "Ngrok token found! Reset? (y/N/O): " RESET_OPTION
     case "$RESET_OPTION" in
         [yY]*)
             read -p "Enter Ngrok token: " NGROK_TOKEN
-            echo $NGROK_TOKEN > $SCRIPT_DIR/ngrok_data.txt
+            echo $NGROK_TOKEN > ngrok_data.txt
             ;;
         *)
-            NGROK_TOKEN=$(cat $SCRIPT_DIR/ngrok_data.txt)
+            NGROK_TOKEN=$(cat ngrok_data.txt)
             ;;
     esac
 fi
