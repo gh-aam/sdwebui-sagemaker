9f1ecaf1d903595022f09abba0a2759354a42e63
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-10 06:42:14 +0600

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(pwd)
+
+cd stable-diffusion-webui
+git pull
+
+if [ ! -f "$SCRIPT_DIR/ngrok_data.txt" ]; then
+    read -p "Enter Ngrok token: " NGROK_TOKEN
+    echo $NGROK_TOKEN > $SCRIPT_DIR/ngrok_data.txt
+else
+    read -p "Ngrok token found! Reset? (y/N/O): " RESET_OPTION
+    case "$RESET_OPTION" in
+        [yY]*)
+            read -p "Enter Ngrok token: " NGROK_TOKEN
+            echo $NGROK_TOKEN > $SCRIPT_DIR/ngrok_data.txt
+            ;;
+        *)
+            NGROK_TOKEN=$(cat $SCRIPT_DIR/ngrok_data.txt)
+            ;;
+    esac
+fi
+
+eval "$(conda shell.bash hook)"
+conda activate automatic
+python launch.py --ngrok $NGROK_TOKEN --api --listen --xformers --no-half-vae --enable-insecure-extension-access --gradio-queue --theme dark
